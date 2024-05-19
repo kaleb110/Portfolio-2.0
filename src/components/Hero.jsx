3ed620aad996337a149b4a751fb9a353913e1685
kaleb110 <137324547+kaleb110@users.noreply.github.com>
2024-05-19 22:35:07 +0300

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ const Hero = () => {
       id="home"
     >
       <div className="py-8 sm:py-12 flex flex-col items-start justify-center gap-6">
-        <h1 className="bg-gradient text-[42px] font-bold">
+        <h1 className="bg-gradient text-[48px] font-bold">
           Hi, <br />
           This Is My Portfolio
         </h1>
         <p className="text-slate-400 text-md uppercase max-w-[520px] pb-4">
-          front-end web developer
+          front-end Web Developer
         </p>
         <button className="btn-gradient text-white text-lg px-12 py-3 rounded-full max-sm:w-full">
           Get Started
